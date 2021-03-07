635c092d56999d5d6eab1a6a7a83ab5fec762646
Austin Wu <hw424@cornell.edu>
2021-03-07 23:27:00 +0800

    Update

--- matrix.ml
+++ matrix.ml
@@ -1,12 +1,14 @@
 module Matrix = struct
   type t = { dimensions : int * int; matrix : float list list }
 
+  exception InvalidDimensions
+
   let matrix t = t.matrix
 
   let empty m n =
     {
       dimensions = (m, n);
-      matrix = List.init m (fun x -> List.init n (fun x -> 0.0));
+      matrix = List.init m (fun i -> List.init n (fun j -> 0.0));
     }
 
   let eye n =
@@ -27,5 +29,10 @@ module Matrix = struct
 
   let mult m1 m2 =
     if snd m1.dimensions != fst m2.dimensions then raise InvalidDimensions
-    else failwith "Unimplemented"
+    else
+      let product v1 v2 =
+        List.fold_left ( +. ) 0.0 (List.map2 (fun x1 x2 -> x1 *. x2) v1 v2)
+      in
+      let columns = (transpose m2).matrix in
+      List.map (fun row -> List.map (product row) columns) m1.matrix
 end
