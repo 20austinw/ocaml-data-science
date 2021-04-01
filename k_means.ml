ae83c1f9cf72dd5675f8279a4db1a58ff7a41d81
Austin Wu <hw424@cornell.edu>
2021-04-01 18:56:10 +0800

    k_means in progress

--- k_means.ml
+++ k_means.ml
@@ -1,9 +1,22 @@
-type k = int
-type max_iterations = int
+type point = { tag : int; pos : float * float }
 
-let init_random_centroids x = ()
-let closest_centroid sample centroids = ()
-let create_clusters = ()
-let calc_centroids = ()
-let get_cluster_labels = ()
-let predict x = ()
+let construct lst =
+  let rec helper acc = function
+    | [] -> acc
+    | x :: xs -> helper ({ tag = 0; pos = x } :: acc) xs
+  in
+  helper [] lst
+
+let classify points n max_iter =
+  let centroids =
+    Array.init n (fun i ->
+        { tag = i; pos = (Random.float 20.0, Random.float 20.0) })
+  in
+  let dist p1 p2 =
+    let x1 = fst p1.pos
+    and y1 = snd p1.pos
+    and x2 = fst p2.pos
+    and y2 = snd p2.pos in
+    Float.sqrt (((x2 -. x1) ** 2.) +. ((y2 -. y1) ** 2.))
+  in
+  let find_nearest = 
