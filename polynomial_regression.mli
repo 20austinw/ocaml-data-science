66dd5af76f37a8d9b62fb64b80e35eb693263ecc
Austin Wu <hw424@cornell.edu>
2021-05-17 16:19:06 +0800

    Update

--- polynomial_regression.mli
+++ polynomial_regression.mli
@@ -1,3 +1,5 @@
 val fit : Matrix.t -> Matrix.t -> int -> Matrix.t
-
+(** [fit v1 v2 n] takes in the x-values v1 and y-values v2 as column vectors
+and the degree of polynomial n and returns the weights corresponding to each
+term *)
 val predict : Matrix.t -> Matrix.t
